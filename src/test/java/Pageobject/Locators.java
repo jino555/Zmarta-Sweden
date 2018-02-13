4455f38f8cec32684c9f4e92ab6fc6f3722c5b29
Jino <Jino@LAPTOP-L45CF4AI>
2018-02-13 15:35:43 +0530

    eee

--- src/test/java/Pageobject/Locators.java
+++ src/test/java/Pageobject/Locators.java
@@ -159,7 +159,7 @@ public class Locators {
 		driver.findElement(mob).sendKeys("9745754904");
 		Thread.sleep(3000);
 		driver.findElement(add).click();
-	   driver.findElement(cossn).sendKeys(read.getData(0, 2, 2));
+	   driver.findElement(cossn).sendKeys(read.getData(1, 2, 2));
 		driver.findElement(coemail).sendKeys("qa1@gmail.com");
 		driver.findElement(comob).sendKeys("9747835332");
 		Thread.sleep(3000);
